a5dfcdd30e8807b41647070850c88a7237ca5824
Rather <66881900+bravepinata@users.noreply.github.com>
2024-04-13 20:28:08 -0400

    updating the readme instructions

--- src/public/inc/html/20-instructions.php
+++ src/public/inc/html/20-instructions.php
@@ -1,10 +1,8 @@
 <h2>Step 1: Domain Setup</h2>
 
 <p>
-    &emsp;Add the entries below to your local hosts file, C:\Windows\System32\drivers\etc\hosts.
+    &emsp;Add the entries below to your local .hosts file, C:\Windows\System32\drivers\etc\hosts.
 </p>
-
-<h3>Host File Entries</h3>
 
 <ul class="no-bullets">
 
