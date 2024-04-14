df7ca16480ce5f41be329e7768de09030e9fd184
Rather <66881900+bravepinata@users.noreply.github.com>
2024-04-13 20:44:13 -0400

    update readme instructions

--- src/public/inc/html/60-instructions.php
+++ src/public/inc/html/60-instructions.php
@@ -2,12 +2,7 @@
 
 <p>
     &emsp;Congratulations! You have built a local development server, configured the sites,
-    and connected to the databases. Go take a break.
-</p>
-
-<p>
-    &emsp;In the <strong>Next Steps</strong>, we will set up the Git repos, import the source 
-    code, and get the websites installed locally.
+    and connected to the databases.
 </p>
 
 <hr/>
